5e175e461a44683061a88f55d5bd2e21a57204ad
MamadTvl <mtavallai80@gmail.com>
2022-12-13 16:10:09 +0330

    update nginx conf

--- v2ray-nginx-cdn/vmess.py
+++ v2ray-nginx-cdn/vmess.py
@@ -9,14 +9,14 @@ path = Path(__file__).parent
 config_file = open(str(path.joinpath('v2ray/config/config.json')), 'r', encoding='utf-8')
 config = json.load(config_file)
 
-caddy = open(str(path.joinpath('caddy/Caddyfile')), 'r', encoding='utf-8').read()
+# caddy = open(str(path.joinpath('caddy/Caddyfile')), 'r', encoding='utf-8').read()
 
 uuid = config['inbounds'][0]['settings']['clients'][0]['id']
-domain = caddy[:caddy.find(' {')]
+domain = input('enter your domain (example.com)')
 
 j = json.dumps({
-    "v": "2", "ps": domain, "add": domain, "port": "443", "id": uuid, "aid": "0", "net": "ws", "type": "none",
-    "host": domain, "path": "/ws", "tls": "tls"
+    "v": "2", "ps": f"<{domain}>", "add": f"<{domain}>", "port": "443", "id": uuid, "aid": "0", "net": "ws", "type": "none",
+    "host": f"<{domain}>", "path": "/ws", "tls": "tls"
 })
 
 print("vmess://" + base64.b64encode(j.encode('ascii')).decode('ascii'))
